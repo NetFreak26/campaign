855a53a0779a3d7ecf28101c1d4baeb48fe12505
NetFreak26 <chirayugarg001999@gmail.com>
2023-02-05 14:31:26 +0530

    campaign contract created and test written

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,115 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.8.17;
+
+contract CampaignFactory {
+    
+    address[] public campaigns;
+
+    function createCampaign(uint _minimumContribution, uint _target, uint _deadline) public {
+        Campaign campaign = new Campaign(_minimumContribution, _target, _deadline, msg.sender);
+        campaigns.push(address(campaign));
+    }
+}
+
+contract Campaign {
+
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        bool cancelled;
+        uint noOfApprovals;
+        mapping(address=>bool) approvers;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    uint public target;
+    uint public deadline;
+    uint public noOfContributors;
+    uint public totalContribution;
+    mapping(address=>uint) public contributors;
+    Request[] public requests;
+    bool onlyOnce = true;
+
+
+    constructor(uint _minimumContribution, uint _target, uint _deadline, address _manager) {
+        minimumContribution = _minimumContribution;
+        target = _target;
+        deadline = block.timestamp + _deadline;
+        manager = _manager;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can access this function");
+        _;
+    }
+
+    function contribute() public payable {
+        require(block.timestamp <= deadline, "Deadline has expired");
+        require(msg.value >= minimumContribution, "You need to send the minimum contribution amount to part of the campaign");
+        if(contributors[msg.sender] == 0) {
+            noOfContributors++;
+            contributors[msg.sender] = msg.value;
+        } else {
+            contributors[msg.sender] += msg.value;
+        }
+        totalContribution += msg.value;
+    }
+
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyManager {
+
+        Request storage newRequest = requests.push();
+        newRequest.description = _description;
+        newRequest.recipient =  _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.cancelled = false;
+        newRequest.noOfApprovals = 0;
+    }
+
+    function approveRequest(uint _requestIndex) public {
+        require(contributors[msg.sender] >= minimumContribution, "You are not entitled to approve this request");
+        Request storage request = requests[_requestIndex];
+
+        require(request.approvers[msg.sender] == false, "You have already approved");
+
+        request.approvers[msg.sender] = true;
+        request.noOfApprovals++; 
+    }
+
+    function finaliseRequest(uint _requestIndex) public onlyManager {
+        require(block.timestamp > deadline && totalContribution >= target, "You can not approve request now");
+        Request storage request = requests[_requestIndex];
+        require(request.completed == false, "This request is already processed");
+        require(request.cancelled == false, "This request is cancelled");
+        require(request.noOfApprovals > noOfContributors / 2, "Not enought members has approved");
+        require(address(this).balance >= request.value, "Not enough balance in the contract");
+
+        request.recipient.transfer(request.value);
+        request.completed = true;
+    }
+
+    function cancelRequest(uint _requestIndex) public onlyManager {
+        require(requests[_requestIndex].completed == false, "You can not delete a completed request");
+        Request storage request = requests[_requestIndex];
+        request.cancelled = true;
+    }
+
+    function extendDeadline(uint _deadline) public onlyManager {
+        require(block.timestamp > deadline && totalContribution < target, "You can not extend a deadline");
+        require(onlyOnce, "You have already extended deadline");
+        deadline = block.timestamp + _deadline;
+        onlyOnce = true;
+    }
+
+    function refundContribution() public {
+        require(block.timestamp > deadline && totalContribution < target, "You can not get a refund now");
+        require(contributors[msg.sender] > minimumContribution, "You have not contributed anything");
+
+        payable(msg.sender).transfer(contributors[msg.sender]);
+        delete contributors[msg.sender];
+        noOfContributors--;
+    }
+}
---
+++ test/Campaign.test.js
@@ -0,0 +1,218 @@
+const assert = require('assert');
+const ganache = require('ganache');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+
+const compiledCampaignFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+const { on } = require('events');
+
+let accounts;
+let campaignFactory;
+let campaign;
+let campaignAddress;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    campaignFactory = await new web3.eth.Contract(compiledCampaignFactory.abi)
+                        .deploy({data: compiledCampaignFactory.evm.bytecode.object})        
+                        .send({from: accounts[0], gas: 10000000});
+
+    await campaignFactory.methods.createCampaign(1000, 10000, 6).send({
+        from: accounts[1],
+        gas: 10000000
+    });
+
+    campaignAddress = await campaignFactory.methods.campaigns(0).call();
+
+    campaign = await new web3.eth.Contract(compiledCampaign.abi, campaignAddress);
+})
+
+describe('tests Campaign', () => {
+
+    it('deploys the campaignFactory and campaign', () => {
+
+        assert.ok(campaignFactory.options.address);
+        assert.ok(campaign.options.address);
+    })
+
+    it('sets the campaign initial values correct', async () => {
+
+        assert.equal(await campaign.methods.manager().call(), accounts[1]);
+        assert.equal(await campaign.methods.minimumContribution().call(), 1000);
+        assert.equal(await campaign.methods.target().call(), 10000);
+    })
+
+    it('requires the minimum contribution', async () => {
+        let minContributionFlag = false;
+        try {
+            await campaign.methods.contribute().send({
+                from: accounts[2],
+                value: 999,
+                gas: 1000000
+            });;
+        } catch(err) {
+            minContributionFlag = true;
+        }
+        assert(minContributionFlag, true);
+    })
+
+    it('contribute works fine and does not add same contributor again', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[2],
+            value: 1000,
+            gas: 1000000
+        });
+        assert.equal(await campaign.methods.noOfContributors().call(), 1);
+        assert.equal(await campaign.methods.totalContribution().call(), 1000);
+        assert.equal(await campaign.methods.contributors(accounts[2]).call(), 1000);
+
+        await campaign.methods.contribute().send({
+            from: accounts[2],
+            value: 1000,
+            gas: 1000000
+        });
+        assert.equal(await campaign.methods.noOfContributors().call(), 1);
+        assert.equal(await campaign.methods.totalContribution().call(), 2000);
+        assert.equal(await campaign.methods.contributors(accounts[2]).call(), 2000);
+
+        await campaign.methods.contribute().send({
+            from: accounts[3],
+            value: 1000,
+            gas: 1000000
+        });
+        assert.equal(await campaign.methods.noOfContributors().call(), 2);
+        assert.equal(await campaign.methods.totalContribution().call(), 3000);
+        assert.equal(await campaign.methods.contributors(accounts[2]).call(), 2000);
+        assert.equal(await campaign.methods.contributors(accounts[3]).call(), 1000);
+    })
+
+    it('only manager can create a request', async () => {
+        let onlyManager = false;
+        try {
+            await campaign.methods.createRequest("Education", accounts[5], 3000).send({
+                from: accounts[2],
+                gas: 1000000
+            })
+        } catch(err) {
+            onlyManager = true;
+        }
+        assert.equal(onlyManager, true);
+    })
+
+    it('request properties are set properly', async () => {
+        await campaign.methods.createRequest("Education", accounts[5], 3000).send({
+            from: accounts[1],
+            gas: 1000000
+        })
+        let request = await campaign.methods.requests(0).call();
+        assert(request);
+        assert.equal(request.description, 'Education');
+        assert.equal(request.recipient, accounts[5]);
+        assert.equal(request.value, '3000');
+        assert.equal(request.completed, false);
+        assert.equal(request.noOfApprovals, '0');
+    })
+
+    it('only manager can cancel a request', async () => {
+        await campaign.methods.createRequest("Education", accounts[5], 3000).send({
+            from: accounts[1],
+            gas: 1000000
+        })
+        let onlyManager = false;
+        try {
+            await campaign.methods.cancelRequest(0).send({
+                from: accounts[2],
+                gas: 1000000
+            })
+        } catch(err) {
+            onlyManager = true;
+        }
+        assert.equal(onlyManager, true);
+    })
+
+    it('cancel request works fine', async () => {
+        await campaign.methods.createRequest("Education", accounts[5], 3000).send({
+            from: accounts[1],
+            gas: 1000000
+        })
+        let request = await campaign.methods.requests(0).call();
+        assert.equal(request.cancelled, false);
+        await campaign.methods.cancelRequest(0).send({
+            from: accounts[1],
+            gas: 1000000
+        })
+        request = await campaign.methods.requests(0).call();
+        assert.equal(request.cancelled, true);
+    })
+
+    it('only contributor can approve a request', async () => {
+        let onlyContributor = false;
+        await campaign.methods.contribute().send({
+            from: accounts[2],
+            value: 1000,
+            gas: 1000000
+        });
+        await campaign.methods.createRequest("Education", accounts[5], 3000).send({
+            from: accounts[1],
+            gas: 1000000
+        })
+
+        try {
+            await campaign.methods.approveRequest(0).send({
+                from: accounts[1],
+                gas: 1000000
+            })
+        } catch(err) {
+            onlyContributor = true;
+        }
+        assert.equal(onlyContributor, true);
+    })
+
+    it('request properties checked after approving', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[2],
+            value: 1000,
+            gas: 1000000
+        });
+        await campaign.methods.createRequest("Education", accounts[5], 3000).send({
+            from: accounts[1],
+            gas: 1000000
+        })
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[2],
+            gas: 1000000
+        })
+        let request = await campaign.methods.requests(0).call();
+
+        assert.equal(request.noOfApprovals, 1);
+    })
+
+    it('same contributor can not approve again', async () => {
+        let onlyOnce = true;
+        await campaign.methods.contribute().send({
+            from: accounts[2],
+            value: 1000,
+            gas: 1000000
+        });
+        await campaign.methods.createRequest("Education", accounts[5], 3000).send({
+            from: accounts[1],
+            gas: 1000000
+        })
+        try {
+            await campaign.methods.approveRequest(0).send({
+                from: accounts[2],
+                gas: 1000000
+            })
+            await campaign.methods.approveRequest(0).send({
+                from: accounts[2],
+                gas: 1000000
+            })
+        } catch(err) {
+            onlyOnce = false;
+        }
+        assert.equal(onlyOnce, false);
+    })
+})
